b48863ab95c7da05582e1bb7ac97b0459e31e8e4
DrewAumick <71027871+DrewAumick@users.noreply.github.com>
2020-11-12 20:28:26 +0000

    More tweaks to data cleaning for custom train script

--- train.py
+++ train.py
@@ -1,72 +1,58 @@
-from sklearn.linear_model import LogisticRegression
 import argparse
 import os
 import numpy as np
-from sklearn.metrics import mean_squared_error
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
-from azureml.core.run import Run
-from azureml.data.dataset_factory import TabularDatasetFactory
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import OneHotEncoder
+
+import joblib
+from azureml.core import Run, Dataset
+from azureml.core.workspace import Workspace
+from azureml.core.authentication import MsiAuthentication
+
+#from data_prep import get_DDoS_dataset
 run = Run.get_context() 
-
-def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-    x_df["y"] = x_df.y.apply(lambda s: 1 if s == "yes" else 0)
-
-    return x_df
-
-def get_labels_and_data(ds):
-    x_df = clean_data(ds)
-
-    y_df = x_df.pop("y")
-
-    return x_df, y_df
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
+    parser.add_argument('--n_estimators', type=int, default=100, help="Number of trees in the forest")
+    parser.add_argument('--max_depth', type=int, default=None, help="The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.")
+    parser.add_argument('--min_samples_split', type=int, default=2, help="The minimum number of samples required to split an internal node.")
+    parser.add_argument('--min_samples_leaf', type=int, default=1, help="The minimum number of samples required to be at a leaf node.")
+    
     args = parser.parse_args()
+    if args.max_depth == 0:
+        max_depth = None
+    else:
+        max_depth = args.max_depth
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Num Estimators:", np.float(args.n_estimators))
+    run.log("Max Depth:", max_depth)
+    run.log("Min Samples Split:", np.int(args.min_samples_split))
+    run.log("Min Samples Leaf:", np.int(args.min_samples_leaf))
 
-    data_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    workspace = run.experiment.workspace
+    dataset_name = 'Clean DDoS Dataset'
+    dataset = Dataset.get_by_name(workspace=workspace, name=dataset_name)
 
-    ds = TabularDatasetFactory.from_delimited_files(path=data_path)
+    df = dataset.to_pandas_dataframe()
 
-    x, y = get_labels_and_data(ds)
+    y = df.pop("Label")
 
-    x_train, x_test, y_train, y_test = train_test_split(x,y)
+    x_train, x_test, y_train, y_test = train_test_split(df,y)
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = RandomForestClassifier(n_estimators=args.n_estimators, 
+                                   max_depth=max_depth, 
+                                   min_samples_split=args.min_samples_split, 
+                                   min_samples_leaf=args.min_samples_leaf, 
+                                   )
+    
+    model = model.fit(x_train,y_train)
 
     joblib.dump(model, './outputs/model.joblib')
 
